42b8b5f98df53601c0d9152e2d5b664eb78abe45
Petr Meca <petrmeca2@gmail.com>
2017-09-09 21:54:39 +0200

    pokročil jsem dál

--- hello_world.py
+++ hello_world.py
@@ -1,44 +1,79 @@
 from random import randrange
 
-pc = randrange(0,3)
-human = int(input("Zadej cislo (0,1 nebo 2 "))
-kamen = 0
-nuzky = 1
-papir = 2
+class _Getch:
+    """Gets a single character from standard input.  Does not echo to the
+screen."""
+    def __init__(self):
+        try:
+            self.impl = _GetchWindows()
+        except ImportError:
+            self.impl = _GetchUnix()
 
-if pc == kamen:
-    while human != papir:
+    def __call__(self): return self.impl()
+
+
+class _GetchUnix:
+    def __init__(self):
+        import tty, sys
+
+    def __call__(self):
+        import sys, tty, termios
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+
+class _GetchWindows:
+    def __init__(self):
+        import msvcrt
+
+    def __call__(self):
+        import msvcrt
+        return msvcrt.getch()
+
+getch = _Getch()
+
+
+def hra():
+    pc = randrange(0, 3)
+    human = int(input("Zadej cislo (0 - kámen, 1 - nůžky ,2 - papír)"))
+    kamen = 0
+    nuzky = 1
+    papir = 2
+    if pc == kamen:
         if human == kamen:
             print("eště ráz1")
-            break
-
         elif human == nuzky:
             print("skoro1")
-            break
         elif human == papir:
-            break
-    pc = "kamen"
-elif pc == papir:
-    while human != nuzky:
+            pc = "kamen"
+            print("vyhráls, počítač měl", pc)
+    elif pc == papir:
         if human == papir:
             print("eště ráz2")
-            break
         elif human == kamen:
             print("skoro2")
-            break
         elif human == nuzky:
-            break
-    pc = "papir"
-elif pc == nuzky:
-    while human != kamen:
+            pc = "papir"
+            print("vyhráls, počítač měl", pc)
+    elif pc == nuzky:
         if human == nuzky:
             print("eště ráz3")
-            break
         elif human == papir:
             print("skoro3")
-            break
         elif human == kamen:
-            break
-        pc = "nuzky"
+            pc = "nuzky"
+            print("vyhráls, počítač měl", pc)
 
-print("vyhráls, počítač měl",pc)
+
+key = getch
+while key != input(chr(27)):
+    enter = input("Zmáčkni enter pro novou hru")
+    if enter == "":
+        hra()
+    print("nebo esc pro konec.")
